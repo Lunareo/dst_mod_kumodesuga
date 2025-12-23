7bcdf99d287db68f267e3b167c2ba401d93d3fce
LunarSolar <kageno13927@yeah.net>
2025-12-23 18:17:21 +0800

    feat: preset shiro skilltree

--- main/derives.lua
+++ main/derives.lua
@@ -1,21 +1,21 @@
 ---@diagnostic disable: inject-field
 require("prefabs")
 ---@class Derive: Prefab
----@overload fun(parent: string, name: string, postfn: fun(inst:ent):(ent)|nil, assets:(table|nil), deps:(table|nil), force_path_search:any)
+---@overload fun(parent: string, name: string, postfn: fun(inst:ent), assets:(table|nil), deps:(table|nil), force_path_search:any)
 ---@field derive string
 ---@field desc string
 Derive = Class(Prefab, function(self, parent, name, postfn, assets, deps, force_path_search)
     local function fn()
         assert(GLOBAL.Prefabs[parent], string.format("Failed to derive %s from %s: Prefab %s doesn't exist yet",
             name, parent, parent))
-        local inst = Prefabs[parent].fn()
+        local inst = GLOBAL.Prefabs[parent].fn()
         if postfn then
             postfn(inst)
         end
         return inst
     end
     deps = merge(deps, { parent })
-    Prefab._ctor(self, name, fn, assets, deps, force_path_search)
+    GLOBAL.Prefab._ctor(self, name, fn, assets, deps, force_path_search)
     self.derive = parent
 end)
 
--- main/scrapbook_kmds.lua
+++ main/scrapbook_kmds.lua
@@ -2,7 +2,7 @@
 local scrapbookdata_k = {
     aramasa = { name = "aramasa", tex = "spear.tex", type = "item", prefab = "aramasa", weapondamage = "8.5 * 8", finiteuses = 150, build = "swap_spear", bank = "spear", anim = "idle" },
     helheim_beyonder = { name = "helheim_beyonder", tex = "helheim_beyonder.tex", type = "item", prefab = "helheim_beyonder", weapondamage = "25.5 +17", weaponrange = 10, finiteuses = 200, build = "helheim_beyonder_fx", bank = "helheim_beyonder_fx", anim = "idle" },
-    silk_robe = { name = "silk_robe", tex = "silk_robe.tex", type = "item", prefab = "silk_robe", armor = 700, absorb_percent = .7, insulator = 180, insulator_type = "winter", build = "silk_robe", bank = "silk_robe", anim = "idle",  },
+    silk_robe = { name = "silk_robe", tex = "silk_robe.tex", type = "item", prefab = "silk_robe", armor = 700, absorb_percent = .7, insulator = 180, insulator_type = "winter", build = "silk_robe", bank = "silk_robe", anim = "idle", },
     spiderscythe = { name = "spiderscythe", tex = "spiderscythe.tex", type = "item", prefab = "spiderscythe", weapondamage = "42.5 +8.5", weaponrange = 1, finiteuses = 200, build = "spiderscythe", bank = "spiderscythe", anim = "idle" },
 }
 
@@ -15,5 +15,3 @@ local scrapbookdata = require "screens/redux/scrapbookdata"
 for prefab, data in pairs(scrapbookdata_k) do
     scrapbookdata[prefab] = data
 end
-
-return scrapbook_prefabs
