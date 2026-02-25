61e324621ce06983935ae7533942daef7cf463f1
LunarSolar <kageno13927@yeah.net>
2026-02-25 19:11:00 +0800

    LOC sorted

--- main/language.lua
+++ main/language.lua
@@ -1,20 +1,26 @@
 ---@diagnostic disable: lowercase-global, undefined-global
 locale = LOC.GetLocaleCode()
-local map = {
-    zh = "zh",
-    zhr = "zh",
-    ch = "zh",
-    chs = "zh",
-    sc = "zh",
-    zht = "zh",
-    tc = "zh",
-    cht = "zh",
-    chinese = "zh",
+local map_zh = {
+    zh = true,
+    zhr = true,
+    ch = true,
+    chs = true,
+    sc = true,
+    zht = true,
+    tc = true,
+    cht = true,
+    chinese = true,
 }
 
-local base = modrequire "languages/strings"
-if map[locale] then
-    base, ext = modrequire("languages/strings_" .. map[locale]), base
-    merge(base, ext)
+local base = modrequire "languages_kmds/strings"
+if not map_zh[locale] then
+    LoadPOFile("scripts/languages_kmds/strings_en.po", "kmds_en")
 end
 merge(STRINGS, base, true)
+TranslateStringTable(STRINGS)
+
+STRINGS.ACTIONS.USESPELLBOOK.CASTSPELL = STRINGS.ACTIONS.CASTSPELL.GENERIC
+STRINGS.CHARACTER_SURVIVABILITY.shiro = STRINGS.CHARACTER_SURVIVABILITY.wilson
+STRINGS.CHARACTER_SURVIVABILITY.wakaba = STRINGS.CHARACTER_SURVIVABILITY.wanda
+STRINGS.NAMES.HALF_SANITY_VAL = STRINGS.NAMES.HALF_SANITY
+STRINGS.SKILLTREE.SHIRO.DOMINATOR_LOCK_DESC = STRINGS.SKILLTREE.WILSON.WILSON_ALLEGIANCE_LOCK_1_DESC
