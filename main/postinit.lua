77affdac893a8f296d22ed36aab90dca5082caf8
LunarSolar <kageno13927@yeah.net>
2026-03-23 11:39:20 +0800

    fix: fix reticule targeting bug

--- main/postinit.lua
+++ main/postinit.lua
@@ -1,5 +1,5 @@
 hookimport("prefabs", "allplayers", "wakaba", "shiro")
-hookimport("components", "builder", "builder_replica", "sanity", "playercontroller", "combat", "playervision", "drownable")
+hookimport("components", "builder", "builder_replica", "sanity", "playercontroller", "combat", "playervision", "drownable", "aoetargeting")
 hookimport("entity")
 hookimport("stategraphs", "SGwilson")
 hookimport("widgets", "eventannouncer", "itemtile", "redux/loadingwidget", "redux/templates")
